77081d85c8a94ea08483cf7c3cd31378b407706d
sujal-meta <sujal@whitehatengineering.com>
2022-04-26 16:39:40 +0545

    inital commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const { Client } = require("minecraft-launcher-core");
+const launcher = new Client();
+const msmc = require("msmc");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const YAML = require("yaml");
+const process = require("process");
+const configFile = "config.yaml";
+
+var config = {
+  server: "buildtheearth.net",
+};
+
+
+if (fs.existsSync(configFile)) {
+  config = YAML.parse(fs.readFileSync(configFile, "UTF-8"));
+} else {
+  fs.writeFileSync(configFile, YAML.stringify(config));
+}
+
+
+function saveAccount() {
+  fs.writeFileSync(accountFile, JSON.stringify(account));
+}
+
+var account = null;
+var accountFile = "account.json";
+
+if (fs.existsSync(accountFile)) {
+  account = JSON.parse(fs.readFileSync(accountFile, "UTF-8"));
+}
+
+async function logIn() {
+  msmc.setFetch(fetch)
+  msmc.fastLaunch("raw",
+    (update) => {
+      console.log("[MSMC]: " + update.data);
+    }).then((result) => {
+      if (msmc.errorCheck(result)) {
+        console.error("Could not log in: " + result.type);
+        process.exit(1);
+      }
+      account = msmc.getMCLC().getAuth(result);
+      account.type = "msa";
+      saveAccount();
+      launch();
+    }).catch((reason) => {
+      console.error("Could not log in: " + reason);
+      process.exit(1);
+    });
+}
+
+if (account == null) {
+  logIn();
+}
+else {
+  (async () => {
+    if (account.type === "msa") {
+      if (!msmc.validate(account)) {
+        account = msmc.getMCLC().getAuth(await msmc.refresh(account, (update) => {
+          if (update.data) {
+            console.log("[MSMC]: " + update.data);
+          }
+        }));
+        account.type = "msa";
+        saveAccount();
+      }
+    }
+    launch();
+  })();
+}
+
+function launch() {
+  let opts = {
+    authorization: account,
+    custom: false,
+    overrides: {
+      detached: false
+    },
+    root: config.directory,
+    version: {
+      number: config.version,
+      type: "release"
+    },
+    server: {
+      host: 'buildtheearth.net'
+    },
+    memory: config.memory,
+    java: true,
+    javapath: null,
+    screen: true,
+  };
+
+  launcher.launch(opts);
+}
